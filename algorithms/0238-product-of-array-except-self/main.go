ba1cc994796fd3b2d4c46b8c8baf02f79ac8b2d1
clouding <ghost0436@gmail.com>
2020-06-06 02:18:45 +0800

    0238. Product of Array Except Self

---
+++ algorithms/0238-product-of-array-except-self/main.go
@@ -0,0 +1,18 @@
+package main
+
+func productExceptSelf(nums []int) []int {
+	ans := make([]int, len(nums))
+
+	ans[0] = 1
+	for i := 1; i < len(nums); i++ {
+		ans[i] = nums[i-1] * ans[i-1]
+	}
+
+	r := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		ans[i] = ans[i] * r
+		r *= nums[i]
+	}
+
+	return ans
+}
---
+++ algorithms/0238-product-of-array-except-self/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want []int
+	}{
+		{
+			nums: []int{1, 2, 3, 4},
+			want: []int{24, 12, 8, 6},
+		},
+	}
+	for _, tt := range tests {
+		assert.Equal(t, tt.want, productExceptSelf(tt.nums))
+	}
+}
