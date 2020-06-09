4218132e4000fb007c0556659ec570655e1bc9de
clouding <ghost0436@gmail.com>
2020-06-09 23:14:36 +0800

    0234. Palindrome Linked List

---
+++ algorithms/0234-palindrome-linked-list/main.go
@@ -0,0 +1,42 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	if fast != nil {
+		slow = slow.Next
+	}
+
+	slow = reverse(slow)
+	fast = head
+	for slow != nil {
+		if slow.Val != fast.Val {
+			return false
+		}
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return true
+}
+
+func reverse(node *ListNode) *ListNode {
+	var prev *ListNode
+	for node != nil {
+		node.Next, prev, node = prev, node, node.Next
+	}
+	return prev
+}
