fa58ba020c29addc498ca2de9a095fc02b971e94
clouding <ghost0436@gmail.com>
2021-07-27 12:19:06 +0800

    update 0015

--- algorithms/0015-3sum/main.go
+++ algorithms/0015-3sum/main.go
@@ -5,33 +5,34 @@ import (
 )
 
 func threeSum(nums []int) [][]int {
-	var ans [][]int
-	n := len(nums)
-	if n == 0 || n < 3 {
-		return ans
+	var ret [][]int
+	length := len(nums)
+	if length < 3 {
+		return ret
 	}
 
 	sort.Ints(nums)
 
-	for i := 0; i < n-2; i++ {
+	for i := 0; i < length-2; i++ {
 		if nums[i] > 0 {
 			break
-		}
-		if i > 0 && nums[i] == nums[i-1] {
+		} else if i != 0 && nums[i] == nums[i-1] {
 			continue
 		}
 		a := nums[i]
 		start := i + 1
-		end := n - 1
+		end := length - 1
 
 		for start < end {
 			b := nums[start]
 			c := nums[end]
-			if a+b+c == 0 {
-				ans = append(ans, []int{a, b, c})
+			sum := a + b + c
+
+			if sum == 0 {
+				ret = append(ret, []int{a, b, c})
 			}
 
-			if a+b+c < 0 {
+			if sum < 0 {
 				start++
 				for start < end && nums[start] == nums[start-1] {
 					start++
@@ -44,5 +45,6 @@ func threeSum(nums []int) [][]int {
 			}
 		}
 	}
-	return ans
+
+	return ret
 }
